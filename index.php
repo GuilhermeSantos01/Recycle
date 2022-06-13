bb1a6137b37341e4c9b916d8d0387b5f21865328
GabLuc <89027631+G4bLuc@users.noreply.github.com>
2022-06-13 08:20:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,52 +2,51 @@
 <html lang="pt">
 <head>
 				<meta charset="UTF-8">
-	<link rel="stylesheet" href="style.css">
-			<title>PW É F0D4</title>
+	<link rel="stylesheet" href="estilo.css">
+		<script src="script.js"></script>
+			<title>AAAAAAAAA</title>
 </head>
 <body>
 				<header>
 								<nav class="nav-bar">
 	<div class="logo"	>
-					<h1>Recycle</h1>			
+					<h1><img src="https://static.wixstatic.com/media/d6aefa_2a84d9e6954a4a579f0aeb9ea77bf3b5~mv2.png/v1/fill/w_720,h_720,al_c,q_95/d6aefa_2a84d9e6954a4a579f0aeb9ea77bf3b5~mv2.webp"       class="reci">Recycle</h1>			
 	</div>			
 			<div class="nav-list">
 							<ul>
-							<li class="nav-item"><a href="#" class="nav-link">Início</a></li>	
-							<li class="nav-item"><a href="#" class="nav-link">Situação do lixo</a></li>	
-							<li class="nav-item"><a href="#" class="nav-link">Recicle conosco</a></li>	
-							<li class="nav-item"><a href="#" class="nav-link">Quanto ganhar?</a></li>	
-									<li class="nav-item"><a href="#" class="nav-link">Fale conosco</a></li>	
+							<li class="nav-item"><a href="inicio.html" class="nav-link">Início</a></li>	
+							<li class="nav-item"><a href="situacao.html" class="nav-link">Situação do lixo</a></li>	
+							<li class="nav-item"><a href="recicla.html" class="nav-link">Aprenda a reciclar</a></li>	
+							<li class="nav-item"><a href="ganho.html" class="nav-link">Quanto ganhar?</a></li>	
+									<li class="nav-item"><a href="interacao.html" class="nav-link">Fale conosco</a></li>	
 											
 		<div class="login-button">
-					<button><a href ="#">Apoie-nos</a></button>
+					<button><a href ="apoie.html">Apoie-nos</a></button>
 		</ul>
 							</div>
 		<div class="mobile-menu-icon">						
-<button onclick="menuShow"><img class="icon" src="img/white-menu-icon-14.jpg"></button>
+<button onclick="menuShow()"><img  src="https://previews.123rf.com/images/fokaspokas/fokaspokas1809/fokaspokas180900164/108564673-hamburger-menu-web-icon-white-icon-with-shadow-on-transparent-background.jpg?fj=1" class="icon"></button>
 								
 	</div>				
 					
 	<div class="mobile-menu">
 					<ul>
-					<li class="nav-item"><a href="#" class="nav-link">Início</a></li>	
-							<li class="nav-item"><a href="#" class="nav-link">Situação do lixo</a></li>	
-							<li class="nav-item"><a href="#" class="nav-link">Recicle conosco</a></li>	
-								<li class="nav-item"><a href="#" class="nav-link">Quanto ganhar?</a></li>	
-									<li class="nav-item"><a href="#" class="nav-link">Fale conosco</a></li>	
+					<li class="nav-item"><a href="inicio.html" class="nav-link">Início</a></li>	
+							<li class="nav-item"><a href="situacao.html" class="nav-link">Situação do lixo</a></li>	
+							<li class="nav-item"><a href="recicla.html" class="nav-link">Aprenda a reciclar</a></li>	
+								<li class="nav-item"><a href="ganho.html" class="nav-link">Quanto ganhar?</a></li>	
+									<li class="nav-item"><a href="interacao.html" class="nav-link">Fale conosco</a></li>	
 									
 		</ul>
 					<div class="login-button">
-					<button><a href="#">Apoie-nos</a></button>
+					<button><a href="apoie.html">Apoie-nos</a></button>
 					
 	</div>
-							
-							</div>
-			</div>
-            </nav>
-            </header>
-			    <div class="r">
-					<img src="http	s://i0.wp.com/blog.larplasticos.com.br/wp-content/uploads/2020/08/O-que-significa-o-simbolo-da-reciclagem.jpg?w=768&ssl=1"	/>
-            </div>
-			</body>
-            </html> 
+	
+</div>	
+</div>
+			</nav>
+			</header>
+	
+	</body>
+							</html>
