b85c48b446cabaf6c636061c68ae246406de6516
Raissa Daniely <107064831+raissadaniely@users.noreply.github.com>
2022-06-14 15:47:44 -0300

    adicionar feedback

---
+++ feedback/feedback.php
@@ -0,0 +1,25 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="feedback.css">
+     <title>feedback</title>
+ </head>
+ <body>
+     <div class="conteudo">
+        <h2>Nos ajude a melhorar dando sua opinião</h2>
+        <form>
+            <label>Nome:</label>
+            <input type="text" required>
+            <br><br>
+            <label>Comentário:</label>
+            <textarea></textarea>
+            <br><br>
+            <button>Enviar</button>
+        </form>
+     </div>
+     
+ </body>
+ </html>
