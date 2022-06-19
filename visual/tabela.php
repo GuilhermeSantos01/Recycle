4b745485cfaca7e4630be235ab4222a1063998fe
Guilherme Santos <98927390+GuilhermeSantos01@users.noreply.github.com>
2022-06-19 18:27:48 -0300

    Add files via upload

---
+++ visual/tabela.php
@@ -0,0 +1,173 @@
+<html>
+    
+  <head>
+    <title>Quanto Ganhar</title>
+    <link rel="stylesheet" type="text/css" href="table.css" />
+    <link rel="stylesheet" href="style.css">
+  </head>
+  
+  <body>
+  <header>
+								<nav class="nav-bar">
+	<div class="logo"	>
+					<h1><img src="https://static.wixstatic.com/media/d6aefa_2a84d9e6954a4a579f0aeb9ea77bf3b5~mv2.png/v1/fill/w_720,h_720,al_c,q_95/d6aefa_2a84d9e6954a4a579f0aeb9ea77bf3b5~mv2.webp"       class="reci">Recycle</h1>			
+	</div>			
+			<div class="nav-list">
+							<ul>
+							<li class="nav-item"><a href="inicio.php" class="nav-link">Início</a></li>	
+							<li class="nav-item"><a href="situacao.html" class="nav-link">Situação do lixo</a></li>	
+							<li class="nav-item"><a href="reciclar.php" class="nav-link">Aprenda a reciclar</a></li>	
+							<li class="nav-item"><a href="tabela.php" class="nav-link">Quanto ganhar?</a></li>	
+									<li class="nav-item"><a href="feedback.php" class="nav-link">Fale conosco</a></li>		
+											
+		<div class="login-button">
+        <button><a href ="login.php">Apoie-nos</a></button>
+		</ul>
+							</div>
+		<div class="mobile-menu-icon">						
+<button onclick="menuShow()"><img  src="https://previews.123rf.com/images/fokaspokas/fokaspokas1809/fokaspokas180900164/108564673-hamburger-menu-web-icon-white-icon-with-shadow-on-transparent-background.jpg?fj=1" class="icon"></button>
+								
+	</div>				
+					
+	<div class="mobile-menu">
+					<ul>
+					<li class="nav-item"><a href="inicio.html" class="nav-link">Início</a></li>	
+							<li class="nav-item"><a href="situacao.html" class="nav-link">Situação do lixo</a></li>	
+							<li class="nav-item"><a href="recicla.html" class="nav-link">Aprenda a reciclar</a></li>	
+								<li class="nav-item"><a href="ganho.html" class="nav-link">Quanto ganhar?</a></li>	
+									<li class="nav-item"><a href="interacao.html" class="nav-link">Fale conosco</a></li>	
+									
+		</ul>
+					<div class="login-button">
+					<button><a href="apoie.html">Apoie-nos</a></button>
+					
+	</div>
+	
+</div>	
+</div>
+			</nav>
+			</header>
+
+    <h1>Além de você ajudar o mundo separando e reciclando o lixo, você sabia que ainda pode ganhar dinheiro com isso?</h1>
+    <br>
+    <h2>A abaixo você verá uma tabela que levando 1 quilo de um certo tipo de lixo para o Ecoponto mais próximo de você, poderá ganhar uma certa quantidade de dinheiro:</h2>
+    
+     <table class="table" border="1">
+     
+      <tr class="gs">
+  
+      <th>Materiais Recicláveis</th>
+      <th>Preço(R$/Kg)</th>
+    
+      </tr>
+         
+      <tr>
+          <th>Vidro</th>
+          <td>R$ 0,04</td>
+      </tr>
+      
+      <tr>
+          <th>Papelão </th>
+          <td>R$ 0,08</td>
+      </tr>
+      
+      <tr>
+          <th>Papel Branco </th>
+          <td>R$ 0,14</td>
+      </tr>
+      
+      <tr>
+          <th>Papel misto</th>
+          <td>R$ 0,06</td>
+      </tr>
+      
+      <tr>
+          <th>Jornal</th>
+          <td>R$ 0,09</td>
+      </tr>
+      
+      <tr>
+          <th>Tetra pak</th>
+          <td>R$ 0,07</td>
+      </tr>
+      
+      <tr>
+          <th>Plástico filme</th>
+          <td>R$ 0,32</td>
+      </tr>
+      
+      <tr>
+          <th>PET</th>
+          <td>R$ 0,40</td>
+      </tr>
+      
+      <tr>
+          <th>PVC</th>
+          <td>R$ 0,40</td>
+      </tr>
+      
+      <tr>
+          <th>Aço Inox 304</th>
+          <td>R$ 1,50</td>
+      </tr>
+      
+      <tr>
+          <th>Aço inox 430</th>
+          <td>R$ 0,30</td>
+      </tr>
+      
+      <tr>
+          <th>Panela de alumínio</th>
+          <td>R$ 2,20</td>
+      </tr>
+      
+      <tr>
+          <th>Alumínio fundindo</th>
+          <td>R$ 1,30</td>
+      </tr>
+      
+      <tr>
+          <th>Baterias</th>
+          <td>R$ 1,00</td>
+      </tr>
+      
+      <tr>
+          <th>Chumbo</th>
+          <td>R$ 0,70</td>
+      </tr>
+      
+      <tr>
+          <th>Bronze</th>
+          <td>R$ 4,00</td>
+      </tr>
+      
+      <tr>
+          <th>Lata de alumínio</th>
+          <td>R$ 1,50</td>
+      </tr>
+      
+      <tr>
+          <th>Lata de aço</th>
+          <td>R$ 0,20</td>
+      </tr>
+      
+      <tr>
+          <th>Latão</th>
+          <td>R$ 2,50</td>
+      </tr>
+      
+      <tr>
+          <th>Ferro fundindo</th>
+          <td>R$ 0,20</td>
+      </tr>
+      
+      <tr>
+          <th>Ferro batido</th>
+          <td>R$ 0,10</td>
+      </tr>
+      
+      </table>
+      
+    </body> 
+    
+</html>
