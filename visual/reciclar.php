615c059183fa23ff461b6de6522467a87ff42729
Renan de Castro Rodrigues <89025568+renan-castro@users.noreply.github.com>
2022-06-19 17:48:45 -0300

    Add files via upload

---
+++ visual/reciclar.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta charset="UTF-8">
+		<link rel="stylesheet" href="style.css">
+		<script src="script.js"></script>
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet">
+			<title>Aprenda a Reciclar</title>
+</head>
+<body class="body-reciclar">
+				<header>
+								<nav class="nav-bar">
+	<div class="logo"	>
+					<h1><img src="https://static.wixstatic.com/media/d6aefa_2a84d9e6954a4a579f0aeb9ea77bf3b5~mv2.png/v1/fill/w_720,h_720,al_c,q_95/d6aefa_2a84d9e6954a4a579f0aeb9ea77bf3b5~mv2.webp"       class="reci">Recycle</h1>			
+	</div>			
+			<div class="nav-list">
+							<ul>
+							<li class="nav-item"><a href="inicio.php" class="nav-link">Início</a></li>	
+							<li class="nav-item"><a href="situacao.html" class="nav-link">Situação do lixo</a></li>	
+							<li class="nav-item"><a href="reciclar.php" class="nav-link">Aprenda a reciclar</a></li>	
+							<li class="nav-item"><a href="tabela.php" class="nav-link">Quanto ganhar?</a></li>	
+									<li class="nav-item"><a href="feedback.php" class="nav-link">Fale conosco</a></li>
+		<div class="login-button">
+		<button><a href ="login.php">Apoie-nos</a></button>
+		</ul>
+							</div>
+		<div class="mobile-menu-icon">						
+<button onclick="menuShow()"><img  src="https://previews.123rf.com/images/fokaspokas/fokaspokas1809/fokaspokas180900164/108564673-hamburger-menu-web-icon-white-icon-with-shadow-on-transparent-background.jpg?fj=1" class="icon"></button>
+								
+	</div>				
+					
+	<div class="mobile-menu">
+					<ul>
+					<li class="nav-item"><a href="inicio.html" class="nav-link">Início</a></li>	
+							<li class="nav-item"><a href="situacao.html" class="nav-link">Situação do lixo</a></li>	
+							<li class="nav-item"><a href="recicla.html" class="nav-link">Aprenda a reciclar</a></li>	
+								<li class="nav-item"><a href="reciclar.php" class="nav-link">Quanto ganhar?</a></li>	
+									<li class="nav-item"><a href="interacao.html" class="nav-link">Fale conosco</a></li>	
+									
+		</ul>
+					<div class="login-button">
+					<button><a href="apoie.html">Apoie-nos</a></button>
+					
+	</div>
+	
+</div>	
+</div>
+			</nav>
+			</header>
+	<hr>
+	<div class="reciclar-content">
+		<h1 class="reciclar-titulo"> Aprenda a Reciclar seu lixo ! </h1>
+		
+           <h3 class="reciclar-inicio"> O HÁBITO DE SEPARAR O LIXO, PARA RECICLAGEM, DEIXOU DE SER UMA PRÁTICA DE POUCOS E SE TORNOU UMA NECESSIDADE DE TODOS! <br> <br>
+           A reciclagem permite a redução do número de resíduos orgânicos que mais poluem por causa do CO2 que liberam. <br>
+		   Sem contar, quando o lixo é mal direcionado, infestando ruas, rios e oceanos! </h3>
+
+		   <img src="/Recycle/visual/imagens/lixeiras.webp" class="lixeiras">
+
+			
+		   <ul> <h2 class="reciclar-separacao"> <li>SEPARAÇÃO</li> </h2>  </ul>
+		   <h3 class="reciclar-separacao-inicio"> Separe todo o material que pode ser reciclado, do que não pode: vidros, papéis, plásticos e metais devem ser colocados em outro saco ou container de lixo.
+		   É importante lembrar que o lixo orgânico não deve ser colocado junto do lixo reciclável, apesar de também pode ser reutilizado. <h3>
+
+
+		   <ul> <h2 class="reciclar-lavagem"> <li>LAVAGEM</li> </h2> </ul>
+	
+		   <h3 class="reciclar-lavagem-inicio"> Todo o lixo separado, deve ser lavado e estar seco, para que possa ser reciclado. </h3>
+		   
+			<ul> <h2 class="reciclar-deposito"> <li>DEPÓSITO</li> </h2> </ul>
+			<h3 class="reciclar-deposito-inicio">Com o lixo reciclável separado e limpo, depositar as sacolas ou caixas em  pontos de coleta na rua, nos dias pré-determinados.</h3>
+			<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+			<script src="ajax.js"></script>
+    </div>
+	</body>
+</html>
