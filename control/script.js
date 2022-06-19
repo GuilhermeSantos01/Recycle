ea24350ef68238a6a67adda4b3e5e61cc3649a47
GabLuc <89027631+G4bLuc@users.noreply.github.com>
2022-06-19 18:17:24 -0300

    Add files via upload

---
+++ control/script.js
@@ -0,0 +1,15 @@
+function menuShow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+menuMobile.classList.remove('open');
+document.querySelector('.icon')
+.src ="img/white-menu-icon-14.jpg"
+    }else{				
+     menuMobile.add('open');
+document.querySelector('.icon') 
+.src ="img/close_white_36dp.svg"
+
+
+} 
+}
+
