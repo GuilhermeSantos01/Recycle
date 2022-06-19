92f397c79d95c04a29e481fb8460dfad24fa72f8
GabLuc <89027631+G4bLuc@users.noreply.github.com>
2022-06-19 19:06:45 -0300

    Add files via upload

---
+++ visual/Index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+				<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css">
+			<title>Home</title>
+</head>
+
+	<body class="body-index">
+<div class="container">
+	
+<header>
+								<nav class="nav-bar">
+	<div class="logo"	>
+					<h1><img src="https://static.wixstatic.com/media/d6aefa_2a84d9e6954a4a579f0aeb9ea77bf3b5~mv2.png/v1/fill/w_720,h_720,al_c,q_95/d6aefa_2a84d9e6954a4a579f0aeb9ea77bf3b5~mv2.webp"       class="reci">Recycle</h1>			
+	</div>			
+			<div class="nav-list">
+							<ul>
+							<li class="nav-item"><a href="inicio.php" class="nav-link">Início</a></li>	
+							<li class="nav-item"><a href="situacao.html" class="nav-link">Situação do lixo</a></li>	
+							<li class="nav-item"><a href="reciclar.php" class="nav-link">Aprenda a reciclar</a></li>	
+							<li class="nav-item"><a href="tabela.php" class="nav-link">Quanto ganhar?</a></li>	
+									<li class="nav-item"><a href="feedback.php" class="nav-link">Fale conosco</a></li>	
+											
+		<div class="login-button">
+		<button><a href ="login.php">Apoie-nos</a></button>
+		</ul>
+							</div>
+		<div class="mobile-menu-icon">						
+<button onclick="menuShow()"><img  src="https://previews.123rf.com/images/fokaspokas/fokaspokas1809/fokaspokas180900164/108564673-hamburger-menu-web-icon-white-icon-with-shadow-on-transparent-background.jpg?fj=1" class="icon"></button>
+								
+	</div>				
+					
+	<div class="mobile-menu">
+					<ul>
+					<li class="nav-item"><a href="inicio.html" class="nav-link">Início</a></li>	
+							<li class="nav-item"><a href="situacao.html" class="nav-link">Situação do lixo</a></li>	
+							<li class="nav-item"><a href="recicla.html" class="nav-link">Aprenda a reciclar</a></li>	
+								<li class="nav-item"><a href="ganho.html" class="nav-link">Quanto ganhar?</a></li>	
+									<li class="nav-item"><a href="interacao.html" class="nav-link">Fale conosco</a></li>	
+									
+		</ul>
+					<div class="login-button">
+					<button><a href="apoie.html">Apoie-nos</a></button>
+					
+	</div>
+	
+</div>	
+</div>
+			</nav>
+			</header>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="ajax.js"></script>
+	</body>
+							</html>
