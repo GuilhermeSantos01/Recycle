fd4915c118831c32a56f51a7671cf91d0175d0eb
rebecaluz <107065039+rebecaluz@users.noreply.github.com>
2022-06-20 19:47:05 -0300

    Add files via upload

---
+++ visual/inicio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="body-inicio">
+    <div class="conteudo">
+        <h1 class="bem-vindo"><p>Bem-vindo</p></h1>
+        <h2 class="introducao"><p>Aqui iremos te ajudar a reciclar de forma pratica, facíl e consciente. </p></h2>
+    </div>
+</body>
+</html>
