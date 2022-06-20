e755cfe3781c4bdb4894abf0b1715e7f9f28fa34
GabLuc <89027631+G4bLuc@users.noreply.github.com>
2022-06-20 18:17:57 -0300

    Add files via upload

---
+++ visual/situacao.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/Recycle/visual/situacao.css">
+    <script src="/Recycle/control/situacao.js"></script>
+    <title>Situação do Lixo</title>
+    <link rel="stylesheet" href="menu.css">
+</head>
+
+<body>
+    <header>
+        <nav class="nav-bar">
+            <div class="logo">
+                <img src="https://static.wixstatic.com/media/d6aefa_2a84d9e6954a4a579f0aeb9ea77bf3b5~mv2.png/v1/fill/w_720,h_720,al_c,q_95/d6aefa_2a84d9e6954a4a579f0aeb9ea77bf3b5~mv2.webp"
+                    class="reci">
+                <h1>Recycle</h1>
+            </div>
+            <div class="nav-list">
+                <ul>
+                <li class="nav-item"><a href="inicio.php" class="nav-link">Início</a></li>
+                    <li class="nav-item"><a href="situacao.php" class="nav-link">Situação do lixo</a></li>
+                    <li class="nav-item"><a href="reciclar.php" class="nav-link">Aprenda a reciclar</a></li>
+                    <li class="nav-item"><a href="tabela.php" class="nav-link">Quanto ganhar?</a></li>
+                    <li class="nav-item"><a href="feedback.php" class="nav-link">Fale conosco</a></li>	
+                </ul>
+            </div>
+            <div class="apoie-button">
+            <button><a href="login.php">Apoie-nos</a></button>
+            </div>
+            <div class="mobile-menu-icon">
+                <button onclick="menuShow()"><img src=" https://i.stack.imgur.com/rYaai.png" class="icon"></button>
+            </div>
+        </nav>
+        <div class="mobile-menu">
+            <ul>
+                <li class="nav-item"><a href="#" class="nav-link">Início</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Situação do lixo</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Aprenda a reciclar</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Quanto ganhar?</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Fale conosco</a></li>
+            </ul>
+            <div class="apoie-button">
+                <button><a href="#">Apoie-nos</a></button>
+            </div>
+        </div>
+    </header>
+    <div class="situacao">
+        <br>
+        <h1 style="text-align: center;">Situação do lixo</h1>
+        <br>
+        <h3>
+            <p style="text-align: center;">O lixo ainda é uma das maiores preocupações de todo o mundo, várias metidas são tomadas mas nem sempre
+                adianta, é um ciclo continuo de desperdício, consumo exagerado e falta de consciência</p>
+        </h3>
+        <br>
+        <img src="https://s2.glbimg.com/iBUAx8qu0pxF0Wefgl_e27KdXOw=/e.glbimg.com/og/ed/f/original/2018/08/06/lixaocatadores20080220marcellocasaljragenciabrasil.jpg"
+            style="width: 75%; height: 75%; margin: auto; display: block;">
+        <br>
+        <h3>
+            <p style="text-align: center;">O lixo que mais assombra até hoje ainda é o plástico, ele tá sempre presente em nossas vidas, a todo
+                momento, na verdade, qualquer tipo de lixo mal descartado pode ser prejudicial e a maioria das pessoas
+                não tem consciência do quanto um papel de bala no cháo já piora as coisas e o plástico essencialmente
+                gera cenas assim:</p>
+        </h3>
+        <br>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpdoHALnszzAp5luKD9te72vun7HVQQRnTNV679NUGrpUGsBPBmYrnJQV8&s=10"
+            style="width: 75%; height: 75%; margin: auto; display: block;">
+        <br>
+        <h3>
+            <p style="text-align: center;">Mas existem formas de impedir que mais coisas como a da imagem aconteçam, por meio de campanhas de
+                conscientização para aprender a maneira correta de descartar lixo e ter um consumo mais consciente e
+                principalmente por meio da reciclagem:</p>
+        </h3>
+        <br>
+        <img src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_xbI-o3RvSgfSg2OQbt355pLF1nx9tl7w0L4EGchj3JkWbiF7xFmkp7k&s=10"
+            style="width: 75%; height: 75%; margin: auto; display: block;">
+        <br>
+        <h3>
+            <p style="text-align: center;">Em nosso site você aprende muito mais sobre isso e como pode se tornar nosso apoiador, obrigado por
+                visitar nosso site!</p><br>
+        </h3>
+    </div>
+</body>
+
+</html>
