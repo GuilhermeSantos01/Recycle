0c17edc40cbf7ec42f75aef0e72d42a116b0930f
rebecaluz <107065039+rebecaluz@users.noreply.github.com>
2022-06-20 19:52:39 -0300

    Add files via upload

--- visual/inicio.php
+++ visual/inicio.php
@@ -8,6 +8,40 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body class="body-inicio">
+<header>
+        <nav class="nav-bar">
+            <div class="logo">
+                <img src="https://static.wixstatic.com/media/d6aefa_2a84d9e6954a4a579f0aeb9ea77bf3b5~mv2.png/v1/fill/w_720,h_720,al_c,q_95/d6aefa_2a84d9e6954a4a579f0aeb9ea77bf3b5~mv2.webp"
+                    class="reci">
+                <h1>Recycle</h1>
+            </div>
+            <div class="nav-list">
+                <ul>
+                <li class="nav-item"><a href="inicio.php" class="nav-link">Início</a></li>
+                    <li class="nav-item"><a href="situacao.php" class="nav-link">Situação do lixo</a></li>
+                    <li class="nav-item"><a href="reciclar.php" class="nav-link">Aprenda a reciclar</a></li>
+                    <li class="nav-item"><a href="tabela.php" class="nav-link">Quanto ganhar?</a></li>
+                    <li class="nav-item"><a href="feedback.php" class="nav-link">Fale conosco</a></li>	
+                </ul>
+            </div>
+            <div class="apoie-button">
+            <button><a href="login.php">Apoie-nos</a></button>
+            </div>
+            <div class="mobile-menu-icon">
+                <button onclick="menuShow()"><img src=" https://i.stack.imgur.com/rYaai.png" class="icon"></button>
+            </div>
+        </nav>
+        <div class="mobile-menu">
+            <ul>
+            <li class="nav-item"><a href="situacao.php" class="nav-link">Situação do lixo</a></li>
+                    <li class="nav-item"><a href="reciclar.php" class="nav-link">Aprenda a reciclar</a></li>
+                    <li class="nav-item"><a href="tabela.php" class="nav-link">Quanto ganhar?</a></li>
+                    <li class="nav-item"><a href="feedback.php" class="nav-link">Fale conosco</a></li>	
+            <div class="apoie-button">
+                <button><a href="#">Apoie-nos</a></button>
+            </div>
+        </div>
+    </header>
     <div class="conteudo">
         <h1 class="bem-vindo"><p>Bem-vindo</p></h1>
         <h2 class="introducao"><p>Aqui iremos te ajudar a reciclar de forma pratica, facíl e consciente. </p></h2>
